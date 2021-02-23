dd19db1c59820eda23fce7f644528b7df46863fa
GabrielRioo <gabrielpaivario@gmail.com>
2021-02-22 23:32:00 -0300

    add method get, post and created the server

---
+++ api/src/server.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+
+const app = express(); // inicia função do express
+
+/**
+ * GET - Buscar
+ * POST - Salvar
+ * PUT - Alterar
+ * DELETE - Deletar
+ * PATCH - Alteração especifica
+ */
+
+ // http://localhost:3333/users
+ app.get("/", (request, response) => {
+    //  return response.send("Hello World - NLW 4") // Enviar mensagens
+     return response.json({message: "Hello Worrld - NLW 4"}) // Enviar mensagens em formato json
+});
+
+// 1° param - Rota(Recurso API)
+// 2° param - Request, Response
+app.post("/", (rrequest, response) => {
+    // Recebeu os dados para salvar
+    return response.json({mensage: "Os dados foram salvos com sucesso!"});
+});
+
+app.listen(3333, () => console.log("Server is running!"))  // cria o servidor
