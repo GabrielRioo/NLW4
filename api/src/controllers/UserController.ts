30796977aa7ebf700a1d09dcb5dac50dd0743624
GabrielRioo <gabrielpaivario@gmail.com>
2021-02-27 21:58:24 -0300

    add the logging and create an user

--- api/src/controllers/UserController.ts
+++ api/src/controllers/UserController.ts
@@ -1,11 +1,30 @@
 import {Request, Response} from 'express';
+import { getRepository } from 'typeorm';
+import { User } from "../models/User";
 
 class UserController {
     async create(request: Request, response: Response) {
-        const body =  request.body;
+        const {name, email} =  request.body;
+        const usersRepository = getRepository(User);
 
-        console.log(body);
-        return response.send();
+        // select * from users where email = 'email'
+        const useralreadyExists = await usersRepository.findOne({
+            email
+        })
+
+        if(useralreadyExists){
+            return response.status(400).json({
+                error: "User already exists",
+            });
+        }
+
+        const user = usersRepository.create({
+            name, email
+        })
+
+        await usersRepository.save(user);
+
+        return response.json(user);
     }
 }
 
