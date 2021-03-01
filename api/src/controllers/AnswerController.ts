8697ca24b6c92a9b5bf0ac727f7f2734e071a47a
GabrielRioo <gabrielpaivario@gmail.com>
2021-02-28 22:35:15 -0300

    add the controller AnswerController

---
+++ api/src/controllers/AnswerController.ts
@@ -0,0 +1,30 @@
+import { getCustomRepository } from 'typeorm';
+import { Request, Response} from 'express';
+import { SurveysUsersRepository } from '../repositories/SurveysUsersRepository';
+
+class AnswerController {
+
+    // http://localhost:3333/answers/1?u=ce5ad0f0-824d-48ad-9756-e7a05543281c
+    async execute(request: Request, response: Response){
+        const { value } = request.params;
+        const { u } = request.query;
+
+        const surveysUsersRepository = getCustomRepository(SurveysUsersRepository);
+
+        const surveyUser = await surveysUsersRepository.findOne({
+            id: String(u),
+        });
+
+        if(!surveyUser){
+            return response.status(400).json({
+                error: "Survey User does not exists!",
+            });
+        }
+
+        await surveysUsersRepository.save(surveyUser);
+
+        return response.json(surveyUser);
+    }
+}
+
+export { AnswerController }
